88bc91c0e51e3327f86ba0d04f29afa27ecb8d84
Marcel DiazGranados Roabyo <59396818+marceldgr4@users.noreply.github.com>
2026-01-22 01:32:50 -0500

    update

--- FrontEnd/front/src/api/supabase.ts
+++ FrontEnd/front/src/api/supabase.ts
@@ -1,7 +1,11 @@
 
-import { createClient } from 'supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Supabase URL or Anon Key is not defined in environment variables');
+    }
+    
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
